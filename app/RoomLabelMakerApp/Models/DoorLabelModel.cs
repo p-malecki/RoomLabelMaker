11a8f879e50762d19e611337abc8b724c3389e17
Sebastian Musial (Sebastianyyy) <64799476+Sebastianyyy@users.noreply.github.com>
2024-01-21 03:24:43 +0100

    print logic for FlowDocuments

--- app/RoomLabelMakerApp/Models/DoorLabelModel.cs
+++ app/RoomLabelMakerApp/Models/DoorLabelModel.cs
@@ -1,4 +1,12 @@
 ﻿using Newtonsoft.Json;
+using RoomLabelMakerApp.Views;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
 
 namespace RoomLabelMakerApp.Models;
 
@@ -62,5 +70,33 @@ public class DoorLabelModel
                 }
             }
         }
+    }
+
+    public static void Print(FlowDocument flowDocument)
+    {
+        PrintDialog pd = new PrintDialog();
+        String copyString = XamlWriter.Save(flowDocument);
+        FlowDocument copy = XamlReader.Parse(copyString) as FlowDocument;
+
+        //copy of FlowDocument to not change state of DoorLabel original Flow Document
+        copy.PageHeight = pd.PrintableAreaHeight;
+        copy.PageWidth = pd.PrintableAreaWidth;
+        copy.PagePadding = new Thickness(30);
+        copy.ColumnGap = 0;
+        copy.ColumnWidth = pd.PrintableAreaWidth;
+
+        //Create Window for Print View
+        if (File.Exists("PreviewPrinting.xps"))
+        {
+            File.Delete("PreviewPrinting.xps");
+        }
+
+        var xpsDocument = new XpsDocument("PreviewPrinting.xps", FileAccess.ReadWrite);
+        XpsDocumentWriter writer = XpsDocument.CreateXpsDocumentWriter(xpsDocument);
+        writer.Write(((IDocumentPaginatorSource)copy).DocumentPaginator);
+        var Document = xpsDocument.GetFixedDocumentSequence();
+        xpsDocument.Close();
+        var windows = new PrintPreviewView(Document);
+        windows.ShowDialog();
     }
 }
